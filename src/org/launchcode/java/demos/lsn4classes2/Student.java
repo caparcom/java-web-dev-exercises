5afcee89d4d15f8b70da07726fecce6414ceea1d
Caleb Parga-Combs <calebparga-combs@Calebs-Air.attlocal.net>
2021-09-14 22:03:56 -0500

    Unit testing exercises

--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -1,5 +1,7 @@
 package org.launchcode.java.demos.lsn4classes2;
 
+import java.util.Objects;
+
 public class Student {
 
     private static int nextStudentId = 1;
@@ -30,18 +32,50 @@ public class Student {
 
 
      //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public String getGradeLevel() {
+      if (numberOfCredits <= 29){
+          return "Freshman";
+      } else if(numberOfCredits >= 30 && numberOfCredits <= 59){
+          return "Sophomore";
+      } else if(numberOfCredits >= 60 && numberOfCredits <= 89){
+          return "Junior";
+      } else if (numberOfCredits >= 90){
+          return "Senior";
+      } return "Credit number required";
+    };
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
         // Update the appropriate fields: numberOfCredits, gpa
+        double totalQualityScore = this.gpa * this.numberOfCredits;
+        totalQualityScore += courseCredits * grade;
+        this.numberOfCredits += courseCredits;
+        this.gpa = totalQualityScore/this.numberOfCredits;
     }
-
+            @Override
+            public String toString() {
+                return "Student{" +
+                        "name='" + name + '\'' +
+                        ", studentId=" + studentId +
+                        ", numberOfCredits=" + numberOfCredits +
+                        ", gpa=" + gpa +
+                        '}';
+            }
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
 
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Student student = (Student) o;
+                return getStudentId() == student.getStudentId() && Objects.equals(getName(), student.getName());
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(getName(), getStudentId());
+    }
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
 
@@ -86,4 +120,5 @@ public class Student {
         sally.addGrade(25, 3.8);
         System.out.println(sally);
     }
+
 }
--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -1,11 +1,47 @@
 package org.launchcode.java.demos.lsn5unittesting.test;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.java.demos.lsn5unittesting.main.Car;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class CarTest {
+    Car test_Car;
+
+    @Before
+    public void createCarObject(){
+        test_Car = new Car("Toyota", "Prius", 10, 50);
+    }
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+    @Test
+    public void emptyTest(){
+        assertEquals(10, 10, .001);
+    }
     //TODO: constructor sets gasTankLevel properly
+    @Test
+    public void testInitialGasTank(){
+        assertEquals(10, test_Car.getGasTankLevel(), .001);
+    }
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void tankLevelAccuracyWithinRange(){
+        test_Car.drive(50);
+        assertEquals(9, test_Car.getGasTankLevel(), .001);
+    }
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void tankLevelAccuracyPassedRange(){
+        test_Car.drive(1001);
+        assertEquals(0, test_Car.getGasTankLevel(), .001);
+    }
     //TODO: can't have more gas than tank size, expect an exception
+    @Test (expected = IllegalArgumentException.class)
+    public void testGasOverfill(){
+        test_Car.addGas(50);
+        fail("Hard no. The vehicle can't have more gas than the size of the tank.");
+    }
 
 }
